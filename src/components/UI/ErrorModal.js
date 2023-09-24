9deefec6c248a7df57ac028d29751892c1f5ae41
Berkay Anduv <sygrok@hotmail.com>
2023-09-24 03:30:50 +0300

    WorkingWithPortals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,22 +1,48 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
+//portals start
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+//portals stop
 
 const ErrorModal = (props) => {
   return (
-    <div className={classes.backdrop} onClick={props.onConfirm}>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    //Fragment is the shorthand of React.Fragment
+    <Fragment>
+      {/* portal gets 2 attributes first one is component and second one is where it will be showed */}
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
